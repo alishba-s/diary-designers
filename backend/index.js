96a279277d78c1dcd311b057c2f015b3069b6292
Jaehoon Sung <jaehoon4567@gmail.com>
2022-11-29 00:28:09 -0500

    first-trial to integrate express on the project

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,46 @@
 
 //this is a node package that will help us create a server
 const express = require("express");
-
 //this is the main app. Apps can get complicated. For now, think of it as a place to store your routes
 const app = express();
-
 //this is the port we are going to listen on. Backend programs listen on ports to get information from other computers
 const PORT = 8000;
+
+/**
+ * Remember how before I talked about listening on a port? We do that here. Our app "pauses" and waits for requests to come in.
+ *
+ * These requests can be from browsers, other computers in the cloud, or even your fridge!
+ */
+app.listen(PORT, function () {
+  console.log(`Listening on port ${PORT}`);
+});
+
+//Confidential Data are here!
+const USER_PASSWORD = "ES1012";
+const USER_SAVED_NOTE = {
+  note1: "This is note 1.",
+  note2: "This is note 2.",
+  note3: "this is note 3.",
+};
+
+/**
+ * Post functions to process the requests from frontend.js.
+ */
+app.post("/post", function (req, res) {
+  res.header("Access-Control-Allow-Origin", "*");
+  console.log("Received Password: ");
+  let data = JSON.parse(req.query["data"]);
+  console.log(data);
+  if (data["password"] == USER_PASSWORD) {
+    res.sendFile(__dirname + "/diary.html");
+  } else {
+    res.send(
+      JSON.stringify({
+        passwordBoolean: false,
+      }),
+    );
+  }
+});
 
 /**
  * This is a route. Routes help the computer understand what the user is trying to access.
@@ -21,13 +55,4 @@ app.get("/", function (req, res) {
   res.json({
     ping: "pong",
   });
-});
-
-/**
- * Remember how before I talked about listening on a port? We do that here. Our app "pauses" and waits for requests to come in.
- *
- * These requests can be from browsers, other computers in the cloud, or even your fridge!
- */
-app.listen(PORT, function () {
-  console.log(`Listening on port ${PORT}`);
 });
--- frontend/frontend.js
+++ frontend/frontend.js
@@ -6,6 +6,8 @@
  *
  * If I were to make a request to http://ericmarcantonio.com/api/, the route would be `/api/`.
  */
+const url = "http://localhost:8000/post"; // "http://localhost:3000/post";
+
 $.getJSON("http://localhost:8000/", function (data) {
   document.getElementById("raw").innerHTML = JSON.stringify(data); //this takes the raw json object and converts it into a string
   document.getElementById("key").innerHTML = JSON.stringify(
@@ -14,3 +16,19 @@ $.getJSON("http://localhost:8000/", function (data) {
   // Don't worry too much about this, I just wanted to show you how to access the key of the object
   document.getElementById("value").innerHTML = JSON.stringify(data.ping); //this shows you how to access a value of an object by using its key (in this case `ping`)
 });
+
+function submitPassword() {
+  var password = document.getElementsByTagName("input")[0].value;
+  console.log(password);
+  $.post(
+    url + "?data=" + JSON.stringify({ password: password }),
+    passwordResponse,
+  );
+}
+
+function passwordResponse(data, status) {
+  console.log(data);
+  console.log(status);
+}
+
+//$.post(url + "");
